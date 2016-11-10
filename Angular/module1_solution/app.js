52bc7ad4da2c535f2470a6d423df195abdb7a9de
Felipe Santos <felipefss@gmail.com>
2016-11-10 17:17:45 +0100

    Finalize implementation of module1.

--- Angular/module1_solution/app.js
+++ Angular/module1_solution/app.js
@@ -1,32 +1,37 @@
 (function () {
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope) {
-    $scope.checkItems = function () {
-      if (!$scope.items || $scope.items.length === 0) {
-        $scope.response = 'Please enter data first';
-        return;
-      }
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.checkItems = function () {
+            if (!$scope.items || $scope.items.length === 0) {
+                $scope.response = 'Please enter data first';
+				$scope.respColor = {'color': 'red'};
+				$scope.borderColor = { 'border-color': 'red' };
+                return;
+            }
 
-      var list = $scope.items.split(',');
-      removeEmpties(list);
+            var list = $scope.items.split(',');
+            removeEmpties(list);
 
-      if (list.length < 4) {
-        $scope.response = 'Enjoy!';
-      } else {
-        $scope.response = 'Too much!';
-      }
-    };
+            if (list.length < 4) {
+                $scope.response = 'Enjoy!';
+            } else {
+                $scope.response = 'Too much!';
+            }
+			$scope.respColor = {'color': 'green'};
+			$scope.borderColor = { 'border-color': 'green' };
+        };
 
-    function removeEmpties(list) {
-      for (var i = 0; i < list.length; i++) {
-        if (list[i] === '') {
-          list.splice(i, 1);
-          i--;
+        function removeEmpties(list) {
+            for (var i = 0; i < list.length; i++) {
+                list[i] = list[i].trim();
+                if (list[i] === '') {
+                    list.splice(i, 1);
+                    i--;
+                }
+            }
         }
-      }
     }
-  }
 })();
